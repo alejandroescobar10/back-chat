e6159435ca5cc3fd75edf42e95c2b011c7a4e196
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2024-10-07 15:31:48 -0500

    back

--- server/index.js
+++ server/index.js
@@ -1,52 +1,23 @@
 import express from "express";
-import http from "http";
-import { Server as SocketServer } from "socket.io";
-import cors from "cors";
+import { AblyRest } from "ably/promises";
 
 const app = express();
-const server = http.createServer(app);
-
-// Configurar CORS para permitir solicitudes desde el frontend
-app.use(
-  cors({
-    origin: "https://front-chat-nine.vercel.app", // Permitir solo el frontend (React)
-    //methods: ["GET", "POST"],
-  })
-);
-
-// Prueba de ruta
-app.get("/", (req, res) => {
-  res.send("¡Backend está funcionando!");
+const ably = new AblyRest({
+  key: "6bgz8Q.pc07CQ:rjC34iblLGHkCAcy4YUVArd0gFn0cg4WKVuXgEKsNR4",
 });
 
-// Configurar Socket.io con CORS
-const io = new SocketServer(server, {
-  cors: {
-    origin: "https://front-chat-nine.vercel.app",
-    credentials: true, // Permitir la comunicación desde React
-    methods: ["GET", "POST"],
-    timeout: 10000,
-  },
-  transports: ["websocket", "polling"],
-});
-
-//Conexión de clientes
-io.on("connection", (socket) => {
-  console.log(socket.id); // Verificar la conexión del cliente
-
-  // Escuchar mensajes entrantes
-  socket.on("message", (body) => {
-    console.log(body); // Verificar que el mensaje está siendo recibido
-    // Emitir mensaje a todos los clientes excepto al que lo envió
-    socket.broadcast.emit("message", {
-      body,
-      from: socket.id.slice(6),
-    });
+app.get("/auth", (req, res) => {
+  const clientId = req.query.clientId;
+  ably.auth.createTokenRequest({ clientId }, (err, tokenRequest) => {
+    if (err) {
+      res.status(500).send("Error requesting token: " + err);
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(tokenRequest));
+    }
   });
 });
 
-// Escuchar en el puerto adecuado para Vercel
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`Server on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
